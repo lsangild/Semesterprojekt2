43252b36493e8ff6074770216a2b435422aa7903
Lasse Sangild <lsangild+github@gmail.com>
2016-05-25 23:11:08 +0200

    Endnu simplere...

--- DE2/RecTest.vhd
+++ DE2/RecTest.vhd
@@ -12,16 +12,11 @@ entity RecTest is
 end RecTest;
 
 architecture test of RecTest is
-signal clk_57600, recvalid, lockSignal	: std_logic := '0';	-- recvalid er det 1 eller 0 fra rxvalid, som checker om vi har fået en korrekt string på 9 bit inklusiv parity bit.
+signal clk_57600, recvalid					: std_logic := '0';	-- recvalid er det 1 eller 0 fra rxvalid, som checker om vi har fået en korrekt string på 9 bit inklusiv parity bit.
 signal indata									: std_logic_vector(7 downto 0) := "00000000";
 begin
 gen		: entity work.BaudRateGenerator	port map (	clk => CLOCK_50, reset => KEY(0), clk_baud => clk_57600);
 rec		: entity work.Receiver				port map (	rxd => GPIO_0(0), reset => KEY(0), clk_baud => clk_57600, rxdata => indata, rxvalid => recvalid);
-codelock	: entity work.Code_Lock				port map (	clk => CLOCK_50, reset => KEY(0), codeEntry => recvalid, code => indata, lock => lockSignal,
+codelock	: entity work.Code_Lock				port map (	clk => CLOCK_50, reset => KEY(0), codeEntry => recvalid, code => indata, lock => GPIO_1(0),
 																		err => LEDR(2 downto 1));
-process(lockSignal)
-begin
-	GPIO_1(0) <= lockSignal;
-end process;
-
 end test;
